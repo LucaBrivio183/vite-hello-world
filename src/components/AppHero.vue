6b2821127395cc0b252fedd5e5ec42b791a20666
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-22 17:50:38 +0100

    feat: excercise done

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    data() {
+        return {
+            image: "src/assets/Jumbotron.jpg",
+
+        }
+    }
+}
+</script>
+<template>
+    <img :src="image" alt="" />
+</template>
+<style scoped></style>
